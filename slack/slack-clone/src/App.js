a98d7f0ffbd5df36d715b43a6f78e73f8a10dea5
Waqas Hussain <s113555@ap.be>
2021-02-25 12:21:04 +0100

    structure done

---
+++ slack/slack-clone/src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Chat from './components/Chat';
+import Login from './components/Login';
+import styled from 'styled-components';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Container>
+          <Header/>
+          <Main>
+            <Sidebar/>
+            <Switch>
+
+              <Route path="/room">
+                <Chat>
+
+                </Chat>
+              </Route>
+
+              <Route path = "/">
+                  <Login>
+
+                  </Login>
+              </Route>
+
+            </Switch>
+          </Main>
+
+          
+        </Container>
+       
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
+
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+background: orange;
+display: grid;
+grid-template-rows: 38px auto;
+`
+
+const Main = styled.div`
+background: blue;
+display: grid;
+grid-template-columns: 260px auto
+`
---
+++ slack/slack-clone/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div style={{background: "red"}}>
+            Sidebar
+        </div>
+    )
+}
+
+export default Sidebar
