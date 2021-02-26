77896aadf491d922368b5a69de10120f70a76038
Waqas Hussain <s113555@ap.be>
2021-02-26 10:30:31 +0100

    basic lay-out done

--- slack/slack-clone/src/components/Sidebar.js
+++ slack/slack-clone/src/components/Sidebar.js
@@ -1,11 +1,139 @@
 import React from 'react'
+import styled from 'styled-components';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import {siderbarItems} from '../data/SidebarData';
+import AddIcon from '@material-ui/icons/Add';
+
 
 function Sidebar() {
     return (
-        <div >
-            Sidebar
-        </div>
+        <Container>
+            <WorkspaceContainer>
+                <Name>
+                    groep1
+                </Name>
+                <NewMessage>
+                    <AddCircleOutlineIcon/>
+                </NewMessage>
+            </WorkspaceContainer>
+            <MainChannels>
+                {
+                    siderbarItems.map(item => (
+                        <MainChannelItem>
+                            {item.icons}
+                            {item.text}
+                        </MainChannelItem>
+
+                    ))
+                }
+               
+            </MainChannels>
+            <ChannelsContainer>
+
+                <NewChannelcontainer>
+                    <div>
+                        Channels
+                    </div>
+                    <AddIcon/>
+                </NewChannelcontainer>
+                <ChannelsList>
+                    <Channel>
+                        # Channel 1
+                    </Channel>
+                    <Channel>
+                    # Channel 2
+                    </Channel>
+                </ChannelsList>
+
+            </ChannelsContainer>
+
+        </Container>
     )
 }
 
 export default Sidebar
+
+const Container = styled.div`
+    background: #3F0E40;
+`
+
+const WorkspaceContainer = styled.div`
+    color:white;
+    height: 64px;
+    dispaly: flex;
+    align-items: center;
+    padding-left: 19px;
+    justify-content: space-between;
+    border-bottmom: 1px solid #532753;
+
+    `
+
+const Name = styled.div`
+
+`
+
+const NewMessage = styled.div`
+    width: 36px;
+    height: 36px;
+    background: white;
+    color: #3F0E40;
+    fill: #3F0E40;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 20px;
+    cursor: pointer;
+
+
+`
+
+const MainChannels = styled.div`
+    padding-top: 20px;
+`
+
+const MainChannelItem = styled.div`
+    color: rgb(188,171,188);
+    display: grid;
+    grid-template-columns: 15% auto;
+    height: 28px;
+    align-items: center;
+    padding-left: 19px;
+    cursor: pointer;
+    :hover {
+        background: #350D36;
+    }
+
+`
+
+const ChannelsContainer = styled.div`
+    color: rgb(188,171,188);
+    margin-top: 10px;
+
+    
+`
+
+const NewChannelcontainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 28px;
+    padding-left: 19px;
+    padding-right: 12px;
+
+`
+
+const ChannelsList = styled.div`
+
+`
+
+const Channel = styled.div`
+    height: 28px;
+    display: flex:
+    align-items: center;
+    padding-left: 19px;
+    cursor: pointer;
+    :hover {
+        background: #350D36;
+    }
+`
---
+++ slack/slack-clone/src/data/SidebarData.js
@@ -0,0 +1,39 @@
+import MessageIcon from '@material-ui/icons/Message';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleIcon from '@material-ui/icons/People';
+import AppsIcon from '@material-ui/icons/Apps';
+
+export const siderbarItems= [
+    {
+        icons:<MessageIcon/>,
+        text: "Thread"
+    },
+
+    {
+        icons:<InboxIcon/>,
+        text: "All DMs"
+    },
+
+    {
+        icons:<DraftsIcon/>,
+        text: "Mentions & Reactions"
+    },
+
+    {
+        icons:<BookmarkBorderIcon/>,
+        text: "Save Items"
+    },
+
+    {
+        icons:<PeopleIcon/>,
+        text: "Peoples & Groups"
+    },
+
+    {
+        icons:<AppsIcon/>,
+        text: "More"
+    },
+
+]
